d18382a7c90c936eb9a1b3cda753fa3b5ed0d830
auvipy <auvipy@gmail.com>
2015-11-11 15:59:32 +0600

    added two script using ansible python API

---
+++ scripts/setup_inventory.py
@@ -0,0 +1,20 @@
+import ansible.runner 
+import ansible.playbook 
+import ansible.inventory 
+from ansible import callbacks 
+from ansible import utils import json 
+
+# the fastest way to set up the inventory 
+# hosts list 
+hosts = ["10.11.12.66"] 
+# set up the inventory, if no group is defined then 'all' group is used by default 
+example_inventory = ansible.inventory.Inventory(hosts) 
+pm = ansible.runner.Runner(
+        module_name = 'command', 
+        module_args = 'uname -a', 
+        timeout = 5, 
+        inventory = example_inventory, 
+        subset = 'all' # name of the hosts group ) 
+
+
+out = pm.run()
